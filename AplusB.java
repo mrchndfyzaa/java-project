c128a5117e9ee2ffa89648237784e7e8a636a17e
feyy <31769603+mrchndfyzaa@users.noreply.github.com>
2023-06-24 05:37:04 +0800

    Add files via upload

---
+++ AplusB.java
@@ -0,0 +1,15 @@
+package aplusb;
+import java.util.Scanner;
+
+public class AplusB {
+    static int AplusB(int first_digit, int second_digit) {
+    return first_digit + second_digit;
+    }
+    
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        System.out.println(AplusB(a, b));
+    }
+}
